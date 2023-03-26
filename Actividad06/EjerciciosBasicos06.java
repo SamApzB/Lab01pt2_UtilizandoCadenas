be09a9661283b71cff068fc02eaf8749dd2abd94
SamApzB <samantha.apaza@ucsm.edu.pe>
2023-03-26 15:54:46 -0500

    (Actividad 06): Para la actividad 06 mediante un JOptionPane obtenemos un texto, y tenemos un a cadenaResultante, el objetivo de esta actividad es ingresas frases de forma infinita insertandolo en un cadena vacia, por lo que mientras que el texto igresado no este vacio podemos seguir ingresando mas texto, cuando dejamos el texto en blanco aparecera otra ventana que nos mostrara todos los texto ingresados anteriormente.

---
+++ Actividad06/EjerciciosBasicos06.java
@@ -0,0 +1,18 @@
+package Actividad06;
+import javax.swing.JOptionPane;
+
+public class EjerciciosBasicos06 {
+
+	public static void main(String[] args) {
+		String texto = JOptionPane.showInputDialog(null, "Introduce un texto, cadena vacia para terminar", "Introducir texto", JOptionPane.INFORMATION_MESSAGE);
+		String cadenaResultante = "";
+		
+		while(!texto.isEmpty()) {
+			cadenaResultante += texto;
+			texto = JOptionPane.showInputDialog(null, "Introduce un texto, cadena vacia para terminar", "Introducir texto", JOptionPane.INFORMATION_MESSAGE);
+			
+		}
+		JOptionPane.showMessageDialog(null, cadenaResultante, "Resultado", JOptionPane.INFORMATION_MESSAGE );
+		
+	}
+}
