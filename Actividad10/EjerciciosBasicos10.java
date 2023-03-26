64fc27c491a98b33383266a601c93183b0b1b85d
SamApzB <samantha.apaza@ucsm.edu.pe>
2023-03-26 16:22:28 -0500

    (Actividad 10): En esta actividad nos dan a mostrar el metodo .substring(,) en el cual tenemos que elegir un inicio y un fin dentro del rango que es la cantidad de caracteres existentes en la cadena, por lo que, este metodo nos sirve para extraer cierta parte de nuestro texto eligiendo desde donde hasta donde queremos el texto.

---
+++ Actividad10/EjerciciosBasicos10.java
@@ -0,0 +1,13 @@
+package Actividad10;
+public class EjerciciosBasicos10 {
+
+	public static void main(String[] args) {
+		String cadena = "Hola mundo";
+		
+		String subcadena = cadena.substring(3, 5);
+		
+		System.out.println(subcadena);
+
+	}
+
+}
