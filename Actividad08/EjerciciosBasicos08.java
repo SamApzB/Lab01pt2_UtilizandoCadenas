54d96d86b85a56f2fa0c655485145d551dc9ed09
SamApzB <samantha.apaza@ucsm.edu.pe>
2023-03-26 16:09:25 -0500

    (Actividad 08): Tenemos un codigo muy simple donde vemos la utilidad de la funcion .length() la cual nos devuele el numero de caracteres de un texto.

---
+++ Actividad08/EjerciciosBasicos08.java
@@ -0,0 +1,9 @@
+package Actividad08;
+public class EjerciciosBasicos08 {
+
+	public static void main(String[] args) {
+		String cadena = "Hola mundo";
+		System.out.println("La cadena " + cadena + " tiene " + cadena.length() + " caracteres");
+
+	}
+}
