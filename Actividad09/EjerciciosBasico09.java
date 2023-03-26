c7d739384c95c838709166fbba8ae5ea7235c895
SamApzB <samantha.apaza@ucsm.edu.pe>
2023-03-26 16:14:59 -0500

    (Actividad 09): Para esta actividad nos piden ingresar dos palabras para al final darnos a decir si son iguales o no, para ello hacen uso del .equals que es un comparador de datos de tipo no primitivo como serian los string, entonces si las dos palabras son iguales nos regresa ese resultado y si no los mismo.

---
+++ Actividad09/EjerciciosBasico09.java
@@ -0,0 +1,23 @@
+package Actividad09;
+
+import java.util.Scanner;
+
+public class EjerciciosBasico09 {
+
+	public static void main(String[] args) {
+		Scanner sn= new Scanner(System.in);
+		System.out.println("Escribe la palabra 1:");
+		String palabra1 = sn.next();
+		
+		System.out.println("Escribe la palabra 2:");
+		String palabra2 = sn.next();
+		
+		if(palabra1.equals(palabra2)) {
+			System.out.println("Las palabras son iguales");
+		}
+		else {
+			System.out.println("Las palabras no son iguales");
+		}
+	}
+
+}
